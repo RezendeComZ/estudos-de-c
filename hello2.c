768f87dd7f904d2938329c76ab7037ef7ed56e68
Gabriel Rezende <g@brielrezen.de>
2021-07-11 17:19:28 -0300

    update

---
+++ hello2.c
@@ -0,0 +1,8 @@
+// versão sem retornar 0, pórem ainda iniciando o main com int
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello World!\n");
+}
---
+++ if.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+// #include <stdbool.h>
+
+int main()
+{
+  int age = 31;
+
+  if (age > 40)
+  {
+    printf("Idade maior que 40\n");
+  }
+  else
+  {
+    printf("Idade menor que 40\n");
+  }
+  return 0;
+}
