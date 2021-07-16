727ef38e4bce10f54242799055e9a96a32d81fe6
Gabriel Rezende <g@brielrezen.de>
2021-07-16 13:40:04 -0300

    update

--- if_singLine.c
+++ if_singLine.c
@@ -5,5 +5,6 @@ int main()
 {
   if (true)
     printf("If em uma linha\n"); // o vs code inpede quando eu salvo e joga para a próxima linha // O jeito que tá é uma péssima prática, pq e não tá na mesma linha, parece que pode executar outras coisas, quando na verdade só consegue executar o primeiro comando
+  printf("Isso tá fora do if\n");
   return 0;
 }
