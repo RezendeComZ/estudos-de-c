cd1a3d5066727d1eec9e3da3b8d9811f13dde893
Gabriel Rezende <g@brielrezen.de>
2021-07-19 10:19:45 -0300

    introdução aos loops em C

---
+++ loops.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// initialization // I
+// comparison // SEE
+// update // YOU
+
+int main()
+{
+  for (int i = 1; i <= 10; i++)
+  {
+    if (i < 10)
+    {
+      printf("E é %i,\n", i);
+    }
+    else
+    {
+      printf("E é %i!\n", i);
+    }
+  }
+
+  return 0;
+}
