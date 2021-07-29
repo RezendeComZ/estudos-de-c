208aef4cdabe7e3effb46e0f69cb46996b91f088
Gabriel Rezende <g@brielrezen.de>
2021-07-29 11:55:17 -0300

    define

---
+++ define.c
@@ -0,0 +1,19 @@
+// DEFINE é tipo uma const. Você não consegue mudar o valor dela.
+#include <stdio.h>
+
+#define ZERO 0 // Maiúsculo por padrão, mas não é necessaário
+#define DEZ 10 // não precisa de ;
+#define NOME "Gabriel"
+#define CONTA (10 / 2) // 5
+
+int numeroDez = DEZ;
+char meuNome[] = NOME;
+int dezPor2 = CONTA;
+
+int main()
+{
+  printf("Valor de dez: %d\n", numeroDez);
+  printf("Valor de dez: %d\n", DEZ);
+  printf("Meu nome: %s\n", meuNome);
+  printf("Dez divido por 2: %d\n", dezPor2);
+}
