ea18c12422ff8d581a7af3f85dd86f8e32058078
Gabriel Rezende <g@brielrezen.de>
2021-08-01 17:13:29 -0300

    árvore desenhada

--- codeBlocks/HelloWorld/main.c
+++ codeBlocks/HelloWorld/main.c
@@ -1,8 +1,14 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
+    printf("    *\n");
+    printf("   /|\\ \n");
+    printf("  /_|_\\ \n");
+    printf(" /__|__\\ \n");
+    printf("/___|___\\ \n");
     printf("Hello world!\n");
     return 0;
 }
