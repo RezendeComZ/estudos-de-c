29de050fb282fd85926274f048e0009a3d2e6be5
Gabriel Rezende <g@brielrezen.de>
2021-08-01 21:57:24 -0300

    diferença entre string e char

--- codeBlocks/HelloWorld/main.c
+++ codeBlocks/HelloWorld/main.c
@@ -4,11 +4,14 @@
 
 int main()
 {
+    char nome[] = "Gabriel"; // string, array e aspas duplas
+    char letra = 'a'; // sem array e aspas simples
     printf("    *\n");
     printf("   /|\\ \n");
     printf("  /_|_\\ \n");
     printf(" /__|__\\ \n");
     printf("/___|___\\ \n");
-    printf("Hello world!\n");
+    printf("Hello world! %s\n", nome); // s para String em aspas duplas
+    printf("Letra A: %c", letra); // c para char em aspas simples
     return 0;
 }
