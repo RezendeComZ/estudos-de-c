d8d3faafc97da682c9d5347dc1f75388ba5e2299
Gabriel Rezende <g@brielrezen.de>
2021-08-02 11:12:52 -0300

    update nos comentários do scanf

--- scanf.c
+++ scanf.c
@@ -7,6 +7,6 @@ int main()
   char primeiroNome[20];
   char segundoNome[20];
   printf("Insira seus dois primeiros nomes: ");
-  scanf("%s%s", primeiroNome, segundoNome); // Se você adicionar outro ele irá ignorar
+  scanf("%s%s", primeiroNome, segundoNome); // Se você adicionar outro ele irá ignorar // Se colocar só um ele vai pedir outro nome, é muito específico
   printf("Seu nome: %s %s\n", primeiroNome, segundoNome);
 }
