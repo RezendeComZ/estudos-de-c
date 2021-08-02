7788f44bb1ce64d42f6b9b2974775b01abfae87a
Gabriel Rezende <g@brielrezen.de>
2021-08-02 15:29:35 -0300

    update em pointers

--- pointers_memoryAddresses.c
+++ pointers_memoryAddresses.c
@@ -10,5 +10,11 @@ int main()
 
   printf("Nome: %p\nIdade: %p\n", &nome, &idade); // endereço na memória, ex: 0x7ffeed6b5920
 
+  printf("----\n\n");
+  int *pIdade = &idade;
+
+  printf("pIdade: %p\n", pIdade);   // 0x7ffeebd60918
+  printf("*pIdade: %d\n", *pIdade); // 31 // Dereferencing pointers
+
   return 0;
 }
